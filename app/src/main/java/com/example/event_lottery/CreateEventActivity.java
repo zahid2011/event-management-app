9b2157843f19dadb2c4d190c5cebcc41b55494c1
Shaaf Babar <shaaf.babar@gmail.com>
2024-10-31 15:18:35 -0600

    shaaf

---
+++ app/src/main/java/com/example/event_lottery/CreateEventActivity.java
@@ -0,0 +1,17 @@
+package com.example.event_lottery;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class CreateEventActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+
+
+    }
+}
--- app/src/main/java/com/example/event_lottery/MainActivity.java
+++ app/src/main/java/com/example/event_lottery/MainActivity.java
@@ -1,24 +1,69 @@
+// MainActivity.java
 package com.example.event_lottery;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.event_lottery.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnCreateNewEvent, btnOngoingEvents, btnPastEvents, btnManageFacility, btnLogout;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnCreateNewEvent = findViewById(R.id.btn_create_event);
+        btnOngoingEvents = findViewById(R.id.btn_ongoing_events);
+        btnPastEvents = findViewById(R.id.btn_past_events);
+        btnManageFacility = findViewById(R.id.btn_manage_facility);
+        btnLogout = findViewById(R.id.btn_logout);
+
+        btnCreateNewEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to CreateEventActivity
+                Intent intent = new Intent(MainActivity.this, CreateEventActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnOngoingEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OngoingEventsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnPastEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PastEventsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnManageFacility.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ManageFacilityActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+            }
         });
     }
 }
