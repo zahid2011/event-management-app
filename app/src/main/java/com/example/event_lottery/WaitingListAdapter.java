694146ed7c401987218fcb0da9bca58bfbf32486
SYED SHAHMEER RAHMAN <adreet21@gmail.com>
2024-12-02 01:00:18 -0700

    UI Fix and US 02.02.01

--- app/src/main/java/com/example/event_lottery/WaitingListAdapter.java
+++ app/src/main/java/com/example/event_lottery/WaitingListAdapter.java
@@ -5,38 +5,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import android.widget.ArrayAdapter;
 
-
-import com.example.event_lottery.R;
-
 import java.util.List;
 
+public class WaitingListAdapter extends ArrayAdapter<WaitingListActivity.WaitingListUser> {
 
-public class WaitingListAdapter extends ArrayAdapter<String> {
+    private final Context context;
+    private final List<WaitingListActivity.WaitingListUser> users;
 
-
-    public WaitingListAdapter(Context context, List<String> participantList) {
-        super(context, 0, participantList);
+    public WaitingListAdapter(Context context, List<WaitingListActivity.WaitingListUser> users) {
+        super(context, 0, users);
+        this.context = context;
+        this.users = users;
     }
-
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.participant_list_item, parent, false);
+            convertView = LayoutInflater.from(context).inflate(R.layout.waiting_list_item, parent, false);
         }
 
+        // Get the current user
+        WaitingListActivity.WaitingListUser user = getItem(position);
 
-        TextView participantName = convertView.findViewById(R.id.tv_participant_name);
+        // Bind views
+        TextView tvUsername = convertView.findViewById(R.id.tv_username);
 
-
-        String name = getItem(position);
-        participantName.setText(name);
-
+        tvUsername.setText(user.getName());
 
         return convertView;
     }
